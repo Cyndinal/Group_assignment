2410b26e1d92b2b296e25d9811831cd56a96c6e6
Maxwell Nyarko <nyarkomaxell03@gmail.com>
2025-02-06 15:10:31 -0700

    Initial Changes

--- Group_7.py
+++ Group_7.py
@@ -1,3 +1,5 @@
+from typing import Final
+
 product_name = input("Enter product name: ")
 purchased_quantity = int(input("Enter purchased quantity : "))  #Must be positive integers
 
@@ -12,42 +14,45 @@ if purchased_quantity > 0:
             selling_price_per_product = float(input("Selling price per item: "))  #must be positive floating-point numbers
             if type(selling_price_per_product) == float and selling_price_per_product>0.0:
                 province = input("Enter province: ").upper()  #must be one of the listed provinces
-                if province == "ON":
-                    GST_or_HST = 13
-                    total_purchase_cost = purchased_quantity * cost_per_product
-                    if sold_quantity > 100:
-                        discounted_rate = discounted_rate_1
-                    elif sold_quantity > 200:
-                        discounted_rate = discounted_rate_2
-                    else:
-                        discounted_rate =0
-                        print("No Discount")
+                # Changes must be made here since you considering all the provinces
+                #  Use case match syntax
 
-                    discounted_selling_price_per_product = selling_price_per_product * (1 - discounted_rate)
-                    total_revenue_excluding_GST_or_HST = sold_quantity * discounted_selling_price_per_product
-                    GST_or_HST_amount = total_revenue_excluding_GST_or_HST * discounted_rate
-                    total_revenue_including_GST_or_HST = total_revenue_excluding_GST_or_HST + GST_or_HST
-                    profit_or_loss = total_revenue_excluding_GST_or_HST - total_purchase_cost
-                    print("*********************************************************")
-                    print("*** Final Report ***")
-                    print("*********************************************************")
-                    print("Category \t Value")
-                    print("Product Name: ", product_name)
-                    print("Province: ", province)
-                    print("Purchased Quantity: ", purchased_quantity)
-                    print("Sold Quantity: ", sold_quantity)
-                    print("Remaining Quantity")
-                    print("Total purchase cost: ", total_purchase_cost)
-                    print("Discount Rate: ", discounted_rate_1*100,"%")
-                    print("Discounted_per_product: ", discounted_selling_price_per_product)
-                    print("Total Revenue excluding GST/HST",total_revenue_excluding_GST_or_HST)
-                    print("GST amount",GST_or_HST_amount)
-                    print("Total Revenue(Incl. GST)", total_revenue_including_GST_or_HST)
-                    print("No profit or loss",profit_or_loss)
-                    print("************************************************************")
-
-                else:
-                    print("Province must be ON")
+                # if province == "ON":
+                #     GST_or_HST = 13
+                #     total_purchase_cost = purchased_quantity * cost_per_product
+                #     if sold_quantity > 100:
+                #         discounted_rate = discounted_rate_1
+                #     elif sold_quantity > 200:
+                #         discounted_rate = discounted_rate_2
+                #     else:
+                #         discounted_rate =0
+                #         print("No Discount")
+                #
+                #     discounted_selling_price_per_product = selling_price_per_product * (1 - discounted_rate)
+                #     total_revenue_excluding_GST_or_HST = sold_quantity * discounted_selling_price_per_product
+                #     GST_or_HST_amount = total_revenue_excluding_GST_or_HST * discounted_rate
+                #     total_revenue_including_GST_or_HST = total_revenue_excluding_GST_or_HST + GST_or_HST
+                #     profit_or_loss = total_revenue_excluding_GST_or_HST - total_purchase_cost
+                #     print("*********************************************************")
+                #     print("*** Final Report ***")
+                #     print("*********************************************************")
+                #     print("Category \t Value")
+                #     print("Product Name: ", product_name)
+                #     print("Province: ", province)
+                #     print("Purchased Quantity: ", purchased_quantity)
+                #     print("Sold Quantity: ", sold_quantity)
+                #     print("Remaining Quantity")
+                #     print("Total purchase cost: ", total_purchase_cost)
+                #     print("Discount Rate: ", discounted_rate_1*100,"%")
+                #     print("Discounted_per_product: ", discounted_selling_price_per_product)
+                #     print("Total Revenue excluding GST/HST",total_revenue_excluding_GST_or_HST)
+                #     print("GST amount",GST_or_HST_amount)
+                #     print("Total Revenue(Incl. GST)", total_revenue_including_GST_or_HST)
+                #     print("No profit or loss",profit_or_loss)
+                #     print("************************************************************")
+                #
+                # else:
+                #     print("Province must be ON")
             else:
                 print("Invalid input, the value must be positive")
         else:
